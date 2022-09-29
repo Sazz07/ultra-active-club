8b683f0b6995086e97898ca59fc48180712b69b0
Sazz07 <sazzad.hossain882@gmail.com>
2022-09-29 22:10:51 +0600

    Bonus part Question1 component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Main from './components/Main/Main';
+import Question1 from './components/Question1/Question1';
 
 function App() {
   return (
     <div className="App">
-    <Main></Main>
+    <div>
+      <Main></Main>
+    </div>
+    <div className='mt-20 w-10/12 mx-auto shadow-2xl rounded-lg p-5'>
+      <Question1></Question1>
+    </div>
+
     </div>
   );
 }
---
+++ src/components/Question1/Question1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Question1 = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold mb-2">How does React work?</h1>
+            <p>            
+            ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.
+            </p>
+        </div>
+    );
+};
+
+export default Question1;
